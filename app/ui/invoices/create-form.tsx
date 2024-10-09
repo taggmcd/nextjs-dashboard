7a9f524ad1a7e8cab8c2846c9100da37339cf64e
Tagg McDonald <taggmcd@gmail.com>
2024-10-08 21:26:49 -0600

    Remove the check attribute from radio button

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -88,8 +88,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-                  checked
-                />
+                  />
                 <label
                   htmlFor="pending"
                   className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
